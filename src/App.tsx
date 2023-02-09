492d5c1fb48adf14282edcb70dd371c95a885316
Greg Creutz <gregory.creutz@gmail.com>
2023-02-09 09:27:25 +0100

    first commmit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+import { Button } from "./Button";
+
+import "./App.css";
+
+// `https://api.github.com/users/:username`;
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="">Intro to bigger projects</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Button.tsx
@@ -0,0 +1,9 @@
+import "./button.css";
+
+export function Button() {
+  return (
+    <>
+      <button className="button-css">Click me</button>
+    </>
+  );
+}
